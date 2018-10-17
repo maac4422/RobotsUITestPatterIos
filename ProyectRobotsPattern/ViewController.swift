d749f493b63d90d6287d5c74f0fa2a40df0a4f5c
Miguel Angel Arenas Correa <miguelarenas98@hotmail.com>
2018-10-16 20:04:00 -0500

    Initial Commit

---
+++ ProyectRobotsPattern/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProyectRobotsPattern
+//
+//  Created by Miguel Angel Arenas Correa on 16/10/18.
+//  Copyright © 2018 Miguel Angel Arenas Correa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

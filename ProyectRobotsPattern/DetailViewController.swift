8765c740cf4b9aaf93db8379c8b50853773ef312
miguel.arenas <miguel.arenas@ceiba.com.co>
2018-10-18 15:15:09 -0500

    Avananzando con la funcionalidad de la app

---
+++ ProyectRobotsPattern/DetailViewController.swift
@@ -0,0 +1,24 @@
+//
+//  DetailViewController.swift
+//  ProyectRobotsPattern
+//
+//  Created by Soporte Ceiba on 10/18/18.
+//  Copyright © 2018 Miguel Angel Arenas Correa. All rights reserved.
+//
+
+import UIKit
+
+
+class DetailViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
--- ProyectRobotsPattern/ViewController.swift
+++ ProyectRobotsPattern/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var usernameTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    @IBOutlet weak var loginBTN: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func login(_ sender: UIButton) {
+        let usernameText = usernameTF!.text
+        let passwordText = passwordTF!.text
+        
+        let correctUser = "user"
+        let correctPassword = "123"
+        if ((usernameText?.isEmpty)! || (passwordText?.isEmpty)!) {
+            generateAlert(title: "Error", message: "The fields must be complete!")
+        }else if (usernameText == correctUser && passwordText == correctPassword){
+            goToDetailPage()
+        }else{
+            generateAlert(title: "Error", message: "User or Password incorrect!")
+        }
+    }
+    
+    func generateAlert(title: String, message: String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alert,animated: true)
+    }
+    
+    func goToDetailPage(){
+        performSegue(withIdentifier: "moveToDetailSegue", sender: self)
+    }
 }
 
